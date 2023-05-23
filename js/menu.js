928a0cc06432eb41f6c793b1fc50efdaee66e94e
ssamueluntu <yakusoku751@gmail.com>
2023-05-23 19:43:19 +0800

    reminder satu

--- js/menu.js
+++ js/menu.js
@@ -23,12 +23,14 @@ const btnHome = document.getElementById("btn-home")
 const btnMyTask = document.getElementById("btn-mytask")
 const btnChat = document.getElementById("btn-chat")
 const btnReport = document.getElementById("btn-report")
+const btnNotif = document.getElementById("btn-notif")
 
 const contentCreate = document.getElementById('container-content')
 const contentHome = document.getElementById('container-home')
 const contentTask = document.getElementById('container-task')
 const contentChat = document.getElementById('container-chat')
 const contentReport = document.getElementById('container-reporting')
+const contentReminder = document.getElementById('container-notif')
 
 
 
@@ -38,12 +40,14 @@ btnCreate.addEventListener("click", function () {
     contentTask.style.display = "none";
     contentReport.style.display = "none";
     contentChat.style.display = "none";
+    contentReminder.style.display = "none";
 });
 btnHome.addEventListener("click", function () {
     contentCreate.style.display = "none";
     contentHome.style.display = "block";
     contentTask.style.display = "none";
     contentReport.style.display = "none";
+    contentReminder.style.display = "none";
     contentChat.style.display = "none";
 });
 btnMyTask.addEventListener("click", function () {
@@ -51,20 +55,31 @@ btnMyTask.addEventListener("click", function () {
     contentHome.style.display = "none";
     contentTask.style.display = "block";
     contentReport.style.display = "none";
+    contentReminder.style.display = "none";
     contentChat.style.display = "none";
 });
 btnChat.addEventListener("click", function () {
     contentCreate.style.display = "none";
     contentHome.style.display = "none";
     contentTask.style.display = "none";
-    contentChat.style.display = "block";
+    contentReminder.style.display = "none";
     contentReport.style.display = "none";
+    contentChat.style.display = "block";
 });
 btnReport.addEventListener("click", function () {
     contentCreate.style.display = "none";
     contentHome.style.display = "none";
     contentTask.style.display = "none";
     contentReport.style.display = "block"
+    contentReminder.style.display = "none";
+    contentChat.style.display = "none";
+});
+btnNotif.addEventListener("click", function () {
+    contentCreate.style.display = "none";
+    contentHome.style.display = "none";
+    contentTask.style.display = "none";
+    contentReport.style.display = "none"
+    contentReminder.style.display = "block";
     contentChat.style.display = "none";
 });
 
